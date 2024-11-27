58a9b11de179b815301de2deba2c41adf2d608ab
drorharpaz <dror.harpaz@post.runi.ac.il>
2024-11-27 16:00:35 +0200

    Anagram - 1

--- Anagram.java
+++ Anagram.java
@@ -6,9 +6,10 @@ public class Anagram {
 		System.out.println(isAnagram("William Shakespeare","I am a weakish speller")); // true
 		System.out.println(isAnagram("Madam Curie","Radium came")); // true
 		System.out.println(isAnagram("Tom Marvolo Riddle","I am Lord Voldemort")); // true
-
+		
 		// Tests the preProcess function.
-		System.out.println(preProcess("What? No way!!!"));
+		System.out.println("preProcess Test: " + preProcess("What? No way!!!"));
+		System.out.println("preProcess Test: " + preProcess("HEY YOU?"));
 		
 		// Tests the randomAnagram function.
 		System.out.println("silent and " + randomAnagram("silent") + " are anagrams.");
@@ -28,22 +29,53 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		StringBuilder sb1 = new StringBuilder(preProcess(str1));
+		StringBuilder sb2 = new StringBuilder(preProcess(str2));
+		if (preProcess(str1).length() != preProcess(str2).length())
+		{
+			return false;	
+		}
+		for (int i = sb1.length() -1; i >= 0; i --)
+		{
+			int j = sb2.toString().indexOf(sb1.charAt(i));
+			if (j != -1)
+			{
+				sb1.deleteCharAt(i);
+				sb2.deleteCharAt(j);
+			}else
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		StringBuilder sb = new StringBuilder(str);
+		String charsToRemove = "!@#$%^&*:()_+-=?><;][]\" ";
+		for (int i = sb.length() - 1; i >= 0; i--) {
+            if (charsToRemove.indexOf(sb.charAt(i)) != -1) {
+                sb.deleteCharAt(i);
+            }
+        }
+		return sb.toString().toLowerCase();
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		StringBuilder sb1 = new StringBuilder(str);
+		StringBuilder sbNew = new StringBuilder();
+		while (sb1.toString().length() > 0)
+		{
+			int index = (int)(Math.random() * sb1.toString().length());
+			char charToReplace = sb1.toString().charAt(index);
+			sb1.deleteCharAt(index);
+			sbNew.append(charToReplace);
+		}
+		return sbNew.toString();
 	}
 }
